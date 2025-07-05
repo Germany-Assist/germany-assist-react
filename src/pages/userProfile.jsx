dfc8c61862d65e6d3cc4cc6a3e16b2bd1aed2944
Muna-Zeer <munaelzeer@gmail.com>
2025-07-05 17:55:35 +0300

    seperation the functions based on the role of the user

--- src/pages/userProfile.jsx
+++ src/pages/userProfile.jsx
@@ -3,25 +3,63 @@ import axios from "axios";
 const UserProfile = ({ userId }) => {
   const [user, setUser] = useState(null);
   const [services, setServices] = useState([]);
-  const [review, setReview] = useState([]);
+  const [reviews, setReviews] = useState([]);
   // const[error,setError]=useState(null);
   const [loading, setLoading] = useState(true);
 
+  const BACKEND_URL = "http://localhost:3000/api";
+  useEffect(() => {
+    fetchProfileDate();
+    fetchUserWithServices();
+    fetchUserWithReviews();
+  }, []);
+
   const fetchProfileDate = async () => {
     try {
-      const userAPI = await axios.get(`api/users/${userId}`);
-      const serviceAPI = await axios.get(`api/service/${userId}`);
-      const reviewAPI = await axios.get(`api/review/${userId}`);
+      const userResponse = await axios.get(`/user/${userId}`, {
+        params: { password: "password123" },
+      });
 
-      setUser(userAPI.data);
-      setServices(serviceAPI.data);
-      setReview(reviewAPI.data);
+
+      const userData = userResponse.data.user;
+      console.log("fetching data of user ", userData);
+      setUser({
+        name: `${userData.firstName}   ${userData.lastName}`,
+        email: userData.email,
+        image: userData.image,
+      });
+
+  
     } catch (error) {
       console.error(`Failed fetching the error ${error}`);
     } finally {
       setLoading(false);
     }
   };
+  const fetchUserWithServices = async () => {
+    try {
+      const serviceResponse = await axios.get(
+        `${BACKEND_URL}/service/${userId}`
+      );
+      setServices(serviceResponse.data);
+    } catch (error) {
+      console.error(`Failed fetching the error ${error}`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchUserWithReviews=async()=>{
+try {
+  const reviewResponse = await axios.get(`${BACKEND_URL}/review/${userId}`);
+       setReviews(reviewResponse.data);
+} catch (error) {
+  console.error(`Failed fetching the error ${error}`);
+    } finally {
+      setLoading(false);
+    }
+}
+  
   if (loading) return <div className="text-center mt-10">Loading...</div>;
   if (!user) return <div className="text-center mt-10"></div>;
   return (
@@ -35,9 +73,8 @@ const UserProfile = ({ userId }) => {
             className="w-24  h-24 rounded-full object-cover "
           />
           <div>
-            <h2 className="text-2xl font-bold ">Name: {user.name}</h2>
-            <p className="text-gray-600">Age: {user.age}</p>
-            <p className="text-gray-600">Job: {user.job}</p>
+            <h2 className="text-2xl font-bold">Name: {user.name}</h2>
+            <p className="text-gray-600">Email: {user.email}</p>
           </div>
           {/* Service section */}
           <div className="bg-white p-4 rounded-xl shadow-md mb-6">
@@ -50,6 +87,26 @@ const UserProfile = ({ userId }) => {
                   <li key={service.id} className="p-3 bg-gray-50">
                     <h4 className="font-medium">{service.title}</h4>
                     <p className="text-gray-600">{service.description}</p>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+
+          {/* Review Section */}
+          <div className="bg-white p-4 rounded-xl shadow-md">
+            <h3 className="text-xl font-semibold mb-4 ">Reviews</h3>
+            {reviews.length === 0 ? (
+              <p className="text-center text-gray-500">No reviews yet.</p>
+            ) : (
+              <ul className="space-y-4">
+                {reviews.map((review) => (
+                  <li key={review.id} className="border p-3 rounded-lg">
+                    <p className="font-medium">{review.reviewName}</p>
+                    <p className="text-gray-700">{review.comment}</p>
+                    <div className="text-yellow-500 ">
+                      Rating:{review.rating / 5}
+                    </div>
                   </li>
                 ))}
               </ul>
