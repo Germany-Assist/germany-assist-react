af1352d5b63aa3269237c9acde11ab3acc3791ed
Muna-Zeer <munaelzeer@gmail.com>
2025-07-14 13:27:02 +0300

    Define the routes in the main page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,21 @@ import './App.css'
 import Signup from "./pages/signup";
 import Login from './pages/login';
 import DashboardHeader from './components/Dashboard/DashboardHeader';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
 
 
   return (
     <>
-    <DashboardHeader/>
+    {/* <DashboardHeader/>
+     */}
+     <Router>
+      <Routes>
+        <Route path="/"  element={<DashboardHeader/>}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
+      </Routes>
+     </Router>
     </>
   )
 }
