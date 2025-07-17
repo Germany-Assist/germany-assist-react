9b310852ae685dce528af4364e98f3272b71b7db
Muna-Zeer <munaelzeer@gmail.com>
2025-07-17 23:34:39 +0300

    Dsiplaying the service cards with horizontal carousal

--- src/components/Services/serviceList.jsx
+++ src/components/Services/serviceList.jsx
@@ -1,17 +1,14 @@
-
-import { services } from "./services.jsx"; 
-import { ServiceCard } from './serviceCard.jsx'; 
+import { services } from "./services.jsx";
+// import { ServiceCard } from './serviceCard.jsx';
+import { ServicesCarousel } from "./serviceCard.jsx";
 
 export const ServiceList = () => {
   return (
-    <div className='container mx-auto px-4 py-8'>
-      <h1 className='text-3xl font-bold text-center mb-12'>Join To Our Services</h1>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
-        {services.map((service) => (
-          <ServiceCard key={service.id} service={service} />
-        ))}
-      </div>
+    <div className="container mx-auto px-4">
+      <h2 className="text-2xl font-bold mb-6 text-blue-500">
+        Popular Services
+      </h2>
+      <ServicesCarousel services={services} />
     </div>
   );
-}
-
+};
