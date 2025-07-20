47d3eb0b3ec7825e9240c25e389b4e578e8585c2
Muna Zeer <92229018+Muna-Zeer@users.noreply.github.com>
2025-07-20 14:11:11 +0300

    5 main dashboard of germany assist (#9)
    * Create header component of germany-assist
    
    * Define the routes in the main page
    
    * Modify the structure of sign up page and fix all the errors
    
    * Enhancing the avatar image and the user info when registering
    
    ---------
    
    Co-authored-by: adruid <ataha@gitlab.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@
 import './App.css'
 import Signup from "./pages/signup";
 import Login from './pages/login';
+import DashboardHeader from './components/Dashboard/DashboardHeader';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import {ServiceList} from './components/Services/serviceList.jsx';
 function App() {
 
 
   return (
     <>
-    {/* <h1>Hello I am Muna</h1>
-      <Signup />
-      <Login /> 
-      <div className="bg-blue-500 text-white p-4 w-300">
-      Tailwind Test Works!
-    </div> */}
-    <ServiceList></ServiceList>
+    {/* <DashboardHeader/>
+     */}
+     <Router>
+      <Routes>
+        <Route path="/"  element={<DashboardHeader/>}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
+      </Routes>
+     </Router>
     </>
   )
 }
