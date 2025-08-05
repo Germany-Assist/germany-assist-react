8db180e1b608a778151258e7ef2b600af4f1e0e3
Muna-Zeer <munaelzeer@gmail.com>
2025-08-05 23:10:38 +0300

    validation data birth of the user

--- src/components/ValidateSignUPInputs.jsx
+++ src/components/ValidateSignUPInputs.jsx
@@ -18,6 +18,25 @@ export const ValidateSignUPInputs = ({
   if (password !== confirmedPassword) errors.push("Password is not matching");
   if (image && !/\.(jpg|jpeg|png|gif)$/.test(image))
     errors.push("Image must end in .jpg, .jpeg, .png, or .gif");
-  if (!DOP) errors.push("Date the of birth is required");
+  if (!DOP) {errors.push("Date the of birth is required")}
+  else{
+      const dateFormat=/^\d{4}-\d{2}-\d{2}$/;
+      if(!dateFormat.test(DOP)){
+        errors.push("Date format is wrong ");
+      }
+      else{
+        const birthDate=new Date(DOP);
+        const today= new Date();
+        const age=today.getFullYear() - birthDate.getFullYear();
+        const monthAge=today.getMonth-birthDate.getMonth();
+        const dayAge= today.getDay-birthDate.getDay();
+
+        const isDateAccepted= monthAge >0 || (monthAge === 0 && dayAge >= 0 );
+        const realAge =  isDateAccepted? age: age-1;
+        if(realAge< 13)
+            errors.push("You must be at least 13 years old");
+            
+      }
+  }
   return errors;
 };
