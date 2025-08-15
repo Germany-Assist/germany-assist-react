21fc5c96167c2daab75a37d7db09a97ec4ca1d04
Muna Zeer <92229018+Muna-Zeer@users.noreply.github.com>
2025-08-15 20:33:58 +0300

    7 sign in the user (#15)
    * modifying the login page with all required coding
    
    * Update README
    
    * 4-Create service details with comments page (#12)
    
    * create service details page with two children components of the service info and comment section
    
    * service info component styling with fetching important details
    
    * component of commentItem structure to calling in the parent component of the comment section
    
    * create comment section components that responsible to generate the comments of the service through the user
    
    * creating the service card of services to display the layout and elements  of the each service
    
    * Listing all the services of the Germany-assist
    
    * Displaying the services cards successfully
    
    * Dsiplaying the service cards with horizontal carousal
    
    * 5 main dashboard of germany assist (#9)
    
    * Create header component of germany-assist
    
    * Define the routes in the main page
    
    * Modify the structure of sign up page and fix all the errors
    
    * Enhancing the avatar image and the user info when registering
    
    ---------
    
    Co-authored-by: adruid <ataha@gitlab.com>
    
    * 3 user dashboard operations (#11)
    
    * fetching the api of getting data from api
    
    * service section info
    
    * seperation the functions based on the role of the user
    
    * correct fetching data from API
    
    * creating the pagination component
    
    * Integrating the pagination component based on the logic of services length and the same for the review section
    
    * Modify the layout of the user info part fetching data well
    
    ---------
    
    Co-authored-by: adruid <ataha@gitlab.com>
    
    * updating routes of dashboard
    
    * create service details page with two children components of the service info and comment section
    
    * Displaying data of the service based on the slug of title in the URL
    
    * displaying the reviews of the service
    
    * enhancing the layout of fethcing the reviews
    
    * Including Review section of the user inside the service details
    
    * Comment out image from user signup request until S3 is ready
    
    ---------
    
    Co-authored-by: adruid <ataha@gitlab.com>
    Co-authored-by: amrdruid <amrdruid@gmail.com>
    
    * 6-sign up the user (#14)
    
    * Create component of the AuthProvider that responsible to access token of  user
    
    * handling the errors in the sign up page
    
    * Validate the  inputs of user when registering within  platform
    
    * validation data birth of the user
    
    * Adding the alert using oast container to handle sign up page
    
    * handle login of the user with required inputs
    
    * Modifying the  auth provider related to the access token and user object
    
    * handle login of the user with required inputs
    
    * handle login of the user with required inputs
    
    * removing debugging statement from login page
    
    ---------
    
    Co-authored-by: amrdruid <amrdruid@gmail.com>
    Co-authored-by: adruid <ataha@gitlab.com>

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,80 +1,44 @@
-import {React,useState} from 'react'
-import InputFields from '../components/InputFields';
-import AuthInputs from '../components/AuthInputs';
-import { AuthContext, useAuth } from './AuthProvider';
+
+import { React, useState } from "react";
+import InputFields from "../components/InputFields";
+import AuthInputs from "../components/AuthInputs";
+import { AuthContext, useAuth } from "./AuthProvider";
 import { ToastContainer, toast } from "react-toastify";
-import axios from 'axios';
+import axios from "axios";
 const LoginPage = () => {
-  const[email,setEmail]= useState('');
-  const[password,setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-
-  const {login}= useAuth();
-  const[loading,setLoading]=useState(false);
-const handleLogin = async () => {
-  if (!email || !password) {
-    toast.error("Please fill in both fields");
-    return;
-  }
-
-
-
-  setLoading(true);
-  try {
-    const response = await axios.post(
-      "http://localhost:3000/api/user/login",
-      {
-        email, 
-        password
-      },
-      { withCredentials: true }
-    );
-
-    console.log("Raw login response:", response.data);
-        console.log("Access token:", response.data.accessToken); 
-      console.log("User object:", response.data.user);
-    if (response.status === 200) {
-      console.log("Access token:", response.data.accessToken); 
-      console.log("User object:", response.data.user);
-      login(response.data.accessToken, response.data.user);
-      toast.success("User login successfully");
+  const { login } = useAuth();
+  const [loading, setLoading] = useState(false);
+  const handleLogin = async () => {
+    if (!email || !password) {
+      toast.error("Please fill in both fields");
+      return;
     }
-  } catch (error) {
-    console.error(error);
-    toast.error(error.response?.data?.message || "Login failed. Try again!");
-  }
-};
+
+    setLoading(true);
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/user/login",
+        {
+          email,
+          password,
+        },
+        { withCredentials: true }
+      );
 
 
+      if (response.status === 200) {
+        login(response.data.accessToken, response.data.user);
+        toast.success("User login successfully");
+      }
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Login failed. Try again!");
+    }
+  };
 
-   
- return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <ToastContainer />
-      <div className="bg-white p-8 rounded shadow-md w-96">
-        <h2 className="text-2xl font-bold mb-6 text-center">Login Page</h2>
+  return (
 
-        <InputFields
-          label="Email "
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <InputFields
-          label="Password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+export default LoginPage;
 
-        <AuthInputs
-          label={loading ? "Logging in..." : "Login"}
-          onClick={handleLogin}
-          disabled={loading}
-        />
-      </div>
-    </div>
-  );
-};
-
-export default LoginPage
