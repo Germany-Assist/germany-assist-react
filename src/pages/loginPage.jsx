f80e0d185caa7ba8a30cfda6ea6b37427d32fc96
Muna-Zeer <munaelzeer@gmail.com>
2025-08-15 18:30:48 +0300

    handle login of the user with required inputs

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -17,39 +17,45 @@ const handleLogin = async () => {
     return;
   }
 
+<<<<<<< HEAD
   
   let emailToSend = email;
   if (!email.includes("@")) {
     emailToSend = `${email}@dummy.com`; 
   }
+=======
+
+>>>>>>> d118b98 (handle login of the user with required inputs)
 
   setLoading(true);
   try {
     const response = await axios.post(
       "http://localhost:3000/api/user/login",
       {
+<<<<<<< HEAD
         email: emailToSend,
+=======
+        email, 
+>>>>>>> d118b98 (handle login of the user with required inputs)
         password
       },
       { withCredentials: true }
     );
 
-    console.log("Login response:", response.data);
-    console.log("Access token:", response.data.accessToken);
-    console.log("User object:", response.data.user);
-
+    console.log("Raw login response:", response.data);
+        console.log("Access token:", response.data.accessToken); 
+      console.log("User object:", response.data.user);
     if (response.status === 200) {
+      console.log("Access token:", response.data.accessToken); 
+      console.log("User object:", response.data.user);
       login(response.data.accessToken, response.data.user);
       toast.success("User login successfully");
     }
   } catch (error) {
     console.error(error);
     toast.error(error.response?.data?.message || "Login failed. Try again!");
-  } finally {
-    setLoading(false);
   }
 };
-
 
 
 
