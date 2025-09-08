bf508e33c208508a18ce02b4ed2ff102af8ac667
Muna-Zeer <munaelzeer@gmail.com>
2025-09-08 22:29:40 +0300

    Define the main attributes anf function to create the coupon

--- src/components/Coupons/CreateCoupon.jsx
+++ src/components/Coupons/CreateCoupon.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import axios from 'axios';
+import { useState } from 'react';
+import { ToastContainer, toast } from "react-toastify";
 
 export const CreateCoupon = () => {
     
@@ -16,7 +18,34 @@ export const CreateCoupon = () => {
         expDate:"",
         ProviderId:"",
     });
+    
+    const handleChange=(e)=>{
+        const {name,value}=e.target;
+        setCoupon({...coupon,[name]:value});
+    };
+
+   const handleSubmit=async(e)=>{
+    e.preventDefault();
+    try {
+        const response = await axios.post("http://localhost:3000/api/coupon", coupon);
+             toast.success("Coupon Card created  successfully");
+             setCoupon(response);
+    } catch (error) {
+       console.error("Failed created the coupon card, Try Again!!",error);
+       toast.error("Failed created the coupon card, Try Again!!"); 
+    }
+   }
+
   return (
-    <div>CreateCoupon</div>
+    <div className='max-w-md mx-auto bg-white shadow-lg rounded-lg p-6 mt-10 border border-blue-500 '>
+    <h2 className='text-2xl font-bold mb-4 text-center text-blue-600'>Create Coupon</h2>
+   <form onSubmit={handleSubmit} className='space-y-4'>
+    <div>
+        <label className='block text-sm font-medium text-gray-700'>Coupon Code</label>
+        <input type="text" name="coupon_code" value={coupon.coupon_code} readOnly className='w-full px-4 py-2 border rounded-lg bg-gray-100 cursor-not-allowed'/>
+    </div>
+    
+   </form>
+    </div>
   )
 }
