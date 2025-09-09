fe206f0ff10788c0c2683840a4c867ce560bc6ae
Muna-Zeer <munaelzeer@gmail.com>
2025-09-09 22:53:54 +0300

    Integrating actual API successfully within sign up page and login page through return the fit alert based on the user using the platform

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,12 +1,12 @@
-import {React,useState} from 'react'
-import InputFields from '../components/InputFields';
-import AuthInputs from '../components/AuthInputs';
-import { AuthContext, useAuth } from './AuthProvider';
+import { React, useState } from "react";
+import InputFields from "../components/InputFields";
+import AuthInputs from "../components/AuthInputs";
+import { AuthContext, useAuth } from "./AuthProvider";
 import { ToastContainer, toast } from "react-toastify";
-import axios from 'axios';
+import axios from "axios";
 const LoginPage = () => {
-  const[email,setEmail]= useState('');
-  const[password,setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
 
   const {login}= useAuth();
@@ -44,14 +44,48 @@ const handleLogin = async () => {
   }
 };
 
+    let emailToSend = email;
+    if (!email.includes("@")) {
+      emailToSend = `${email}@dummy.com`;
+    }
 
+    setLoading(true);
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/user/login",
+        {
+          email: emailToSend,
+          password,
+        },
+        { withCredentials: true }
+      );
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 
 =======
 >>>>>>> d118b98 (handle login of the user with required inputs)
    
  return (
+=======
+      console.log("Login response:", response.data);
+      console.log("Access token:", response.data.accessToken);
+      console.log("User object:", response.data.user);
+
+      if (response.status === 200) {
+        login(response.data.accessToken, response.data.user);
+        toast.success("User login successfully");
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(error.response?.data?.message || "Login failed. Try again!");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+>>>>>>> f60a5b7 (Integrating actual API successfully within sign up page and login page through return the fit alert based on the user using the platform)
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <ToastContainer />
       <div className="bg-white p-8 rounded shadow-md w-96">
@@ -80,4 +114,4 @@ const handleLogin = async () => {
   );
 };
 
-export default LoginPage
+export default LoginPage;
