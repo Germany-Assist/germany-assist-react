00df0d2a9d26181eb55c3f53cdbbb2466196196b
Muna-Zeer <munaelzeer@gmail.com>
2025-09-18 22:51:44 +0300

    rewrite the code of fetching the profile of user

--- src/pages/AuthProvider.jsx
+++ src/pages/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 export const AuthContext = createContext();
 
