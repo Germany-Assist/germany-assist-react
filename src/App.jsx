61d14a1ff99f0ecd1c7d1b96eca5e1c30a576332
Muna-Zeer <munaelzeer@gmail.com>
2025-09-22 00:37:53 +0300

    Display the reviews part of specific service and with it's content

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,12 @@ function App() {
             <Jobs />
           </>
         } ></Route>
+        <Route path="/serviceDetails/:id" element={
+          <>
+            <DashboardHeader />
+            <ServiceDetails />
+          </>
+        } ></Route>
         <Route path="/onboarding" element={<OnboardingPage />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
@@ -46,7 +52,6 @@ function App() {
           </>
         }></Route>
         <Route path="/userProfile" element={<UserProfile />}></Route>
-        <Route path="/serviceDetails/:id" element={<ServiceDetails />}></Route>
       </Routes>
     </Router>
   );
