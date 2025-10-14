0f3debee28679231fb3d7ffa5a36850d5f34fee3
Muna-Zeer <munaelzeer@gmail.com>
2025-10-14 13:25:51 +0300

    fixing calling jwt-decoded token

--- src/pages/AuthProvider.jsx
+++ src/pages/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
-import jwtDecode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 export const AuthContext = createContext();
 
