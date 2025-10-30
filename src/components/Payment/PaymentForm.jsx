ec3c43cc5ec1a7b1e844c182a9255d937bb51d13
Muna-Zeer <munaelzeer@gmail.com>
2025-10-30 15:29:29 +0200

    Integrating Payment method in payment form

--- src/components/Payment/PaymentForm.jsx
+++ src/components/Payment/PaymentForm.jsx
@@ -7,18 +7,40 @@ export const PaymentForm = () => {
   const elements = useElements();
   const [isLoading, setIsLoading] = useState(false);
   const [paid, setPaid] = useState(false);
+  const [paymentMethod, setPaymentMethod] = useState("card");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const { error, paymentIntent } = await stripe.confirmCardPayment(
-      "CLIENT_SECRET_FROM_BACKEND",
-      {
-        payment_method: { card: elements.getElement(CardElement) },
+    setIsLoading(true);
+    if (paymentMethod === "card") {
+      const cardElement = elements.getElement(CardElement);
+      if (cardElement) return;
+
+      const { error, paymentIntent } = await stripe.confirmCardPayment(
+        "CLIENT_SECRET_FROM_BACKEND",
+        {
+          payment_method: {
+            card: cardElement,
+            billing_details: {
+              name: "Test User",
+              email: "test@test.com",
+            },
+          },
+        }
+      );
+      if (error) {
+        alert(error.message);
+      } else if (paymentIntent.status === "succeeded") {
+        setIsLoading(false);
+        window.location.href = "/confirmation";
+      } else {
+        alert(`Redirecting to  ${paymentMethod} ...`);
+        setTimeout(() => {
+          window.location.href = "/confirmation";
+        }, 1500);
       }
-    );
-    if (!error && paymentIntent.status === "succeeded") {
-      window.location.href = "/confirmation";
     }
+    setIsLoading(false);
   };
   return (
     <div>
@@ -29,6 +51,16 @@ export const PaymentForm = () => {
         <h2 className="text-xl font-semibold text-gray-700 text-center mb-4">
           Payment
         </h2>
+          {/* Payment methods */}
+          <div className="space-y-2">
+              <label className="font-medium text-gray-600">Select Payment Method</label>
+              <select value={paymentMethod} onChange={(e)=> setPaymentMethod(e.target.value)} className="w-full border border-gray-300 rounded-xl focus:outline-none">
+                <option value="card">Credit / Debit Card</option>
+                <option value="paypal">Paypal</option>
+                <option value="applepay">Apple Pay</option>
+                <option value="googlepay">Google Pay</option>
+              </select>                  
+          </div>
         <div className="border border-gray-300 rounded-xl p-3 bg-gray-50">
           <CardElement />
         </div>
