8c0a61ec3ff1fdbfa48c5700d0ff0742443ee50f
Amr Imad <amrimadkh@gmail.com>
2026-02-26 22:09:43 +0200

    adding important fix to socket url

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,15 @@
 import { io } from "socket.io-client";
+export const STAGING_BACKEND_URL = import.meta.env.VITE_DROPLET_STAGING_URL;
+export const NODE_ENV = import.meta.env.VITE_NODE_ENV;
+export const LOCAL_BACKEND_URL = import.meta.env.VITE_LOCAL_BACKEND_URL;
+export const DROPLET_BACKEND_URL = import.meta.env.VITE_DROPLET_BACKEND_URL;
 
-export const socket = io("http://localhost:3000/", {
+export const SOCKET_URL =
+  NODE_ENV === "production"
+    ? `${DROPLET_BACKEND_URL}`
+    : NODE_ENV === "staging"
+      ? `${STAGING_BACKEND_URL}`
+      : `${LOCAL_BACKEND_URL}`;
+export const socket = io(SOCKET_URL, {
   autoConnect: false,
 });
