9c001650a91c9d4ab9ff9afeebbb46cd03ce6e47
Rawan Mahmoud <rawanmahmoud323@gmail.com>
2026-03-31 15:03:33 +0300

    save update for sidbar

--- src/components/ui/dashboard/DashboardLayout.jsx
+++ src/components/ui/dashboard/DashboardLayout.jsx
@@ -20,7 +20,7 @@ const DashboardLayout = ({ children }) => {
         <DashboardNav />
         <main className="flex-1 overflow-y-auto p-4 md:p-8">
           {children}
-     <Outlet />
+    <Outlet/>
         </main>
       </div>
     </div>
